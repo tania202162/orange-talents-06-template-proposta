8fff83be31a883219306093ef5372098dcfac13c
unknown <tania.alves@zup.com.br>
2021-07-20 13:52:50 -0300

    Setup da Proposta

---
+++ src/main/java/br/com/zup/academy/tania/proposta/PropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.tania.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaApplication.class, args);
+	}
+
+}
